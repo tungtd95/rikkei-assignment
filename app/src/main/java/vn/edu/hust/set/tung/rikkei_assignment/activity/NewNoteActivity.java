def38256e59f93ba82f29ad7189baec59aa31f68
Tung <tungtd95@gmail.com>
2017-10-20 02:51:24 +0700

    add note

--- app/src/main/java/vn/edu/hust/set/tung/rikkei_assignment/activity/NewNoteActivity.java
+++ app/src/main/java/vn/edu/hust/set/tung/rikkei_assignment/activity/NewNoteActivity.java
@@ -3,8 +3,12 @@ package vn.edu.hust.set.tung.rikkei_assignment.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import vn.edu.hust.set.tung.rikkei_assignment.R;
+import vn.edu.hust.set.tung.rikkei_assignment.util.Echo;
 
 /**
  * Created by Administrator on 20/10/2017.
@@ -17,4 +21,27 @@ public class NewNoteActivity extends AppCompatActivity{
         setContentView(R.layout.activity_new_note);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_new_note, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.itemCamera:
+                Echo.echo("camera");
+                return true;
+            case R.id.itemColor:
+                Echo.echo("color");
+                return true;
+            case R.id.itemSave:
+                Echo.echo("save");
+                return true;
+            default:
+                return false;
+        }
+    }
 }
---
+++ app/src/main/java/vn/edu/hust/set/tung/rikkei_assignment/util/Echo.java
@@ -0,0 +1,13 @@
+package vn.edu.hust.set.tung.rikkei_assignment.util;
+
+import android.util.Log;
+
+/**
+ * Created by Administrator on 20/10/2017.
+ */
+
+public class Echo {
+    public static void echo(String message) {
+        Log.i("main", message);
+    }
+}
